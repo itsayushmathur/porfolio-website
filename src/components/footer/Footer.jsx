de3acc2fedaacdfd6a0abac8313a1f0999ec6ba2
Ayush Mathur <mathurayush121@gmail.com>
2023-06-24 04:28:22 +0530

    Finalized

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,11 +1,67 @@
-import React from 'react'
-import './footer.css'
+import React from "react";
+import "./footer.css";
+import { BsLinkedin } from "react-icons/bs";
+import { FaGithub } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <footer>
+      <a href="#" className="footer__logo">
+        Ayush Mathur
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
 
-export default Footer
+      <div className="footer__socials">
+        <a
+          href="https://github.com/itsayushmathur"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/itsayushmathur/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://www.instagram.com/itsayushmathur/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiFillInstagram />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Ayush Mathur. All rights reserved.</small>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
