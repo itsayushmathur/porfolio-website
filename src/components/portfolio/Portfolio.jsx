ac97414214046e0a42001365f87a496fcbc9400d
Ayush Mathur <mathurayush121@gmail.com>
2023-06-24 13:10:09 +0530

    modifs

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,6 +11,7 @@ const data = [
     image: TAMSimg,
     title: "T A M S",
     github: "https://github.com/itsayushmathur/TAMS",
+    demo: "",
     description:
       "TAMS (Talent Allocation Management System) is a React.js-based project that streamlines talent allocation and management processes within organizations. It offers a user-friendly interface for HR managers and project leads to efficiently search for suitable resources based on skills and availability. TAMS enhances resource allocation, project efficiency, and overall productivity in organizations.",
   },
@@ -19,6 +20,7 @@ const data = [
     image: WonderStudyimg,
     title: "Wonder Study",
     github: "https://github.com/itsayushmathur/WonderStudy-Course_Management",
+    demo: "https://github.com/itsayushmathur/WonderStudy-Course_Management",
     description:
       "WonderStudy is an application program developed in Java, utilizing Swing and AWT, designed to facilitate efficient management of courses, faculty, and students for educational institutes. The project involves creating a user-friendly interface for administrators, teachers, and students to manage course enrollment, track student progress, and generate reports.",
   },
@@ -28,6 +30,7 @@ const data = [
     title: "RTO",
     github:
       "https://github.com/itsayushmathur/road-and-transport-office.github.io",
+    demo: "https://github.com/itsayushmathur/road-and-transport-office.github.io",
     description:
       "Regional Transport Office Management is a Java application that provides various transport-related services, including driving license applications, vehicle registration, and management. The project utilizes Java programming concepts and database connectivity to create a comprehensive system that streamlines the process of managing and tracking transportation-related activities.",
   },
@@ -36,6 +39,7 @@ const data = [
     image: Vaultimg,
     title: "The Vault",
     github: "https://github.com/itsayushmathur/The_Vault.github.io",
+    demo: "https://github.com/itsayushmathur/The_Vault.github.io",
     description:
       "The Vault is a Java-based application designed to manage vehicle catalogs for a car showroom. It consisted of modules for administration, employees, and customers. The application facilitated vehicle booking, catalog display, and provided essential administrative features. Its goal was to enhance the overall management and customer experience within a car showroom environment.",
   },
@@ -47,7 +51,7 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-        {data.map(({ id, image, title, github, description }) => (
+        {data.map(({ id, image, title, github, demo, description }) => (
           <article key={id} className="portfolio__item">
             <div className="portfolio__item-image">
               <img src={image} alt={title} />
@@ -57,6 +61,9 @@ const Portfolio = () => {
             <div className="portfolio__item-cta">
               <a href={github} className="btn" target="_blank" rel="noreferrer">
                 Github
+              </a>
+              <a href={demo} className="btn" target="_blank" rel="noreferrer">
+                Live Demo
               </a>
             </div>
             <h4>{description}</h4>
